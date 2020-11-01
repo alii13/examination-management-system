fa45debd9be21846e0e1c14ab73184b7ae041ea6
alii13 <shekhaliul44@gmail.com>
2020-11-01 16:14:16 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const student = require("./routes/student");
 const InitiateMongoServer = require("./config/db");
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
+var path = require("path");
 require('dotenv').config()
 
 // Initiate Mongo Server
@@ -34,6 +35,15 @@ app.get("/", (req, res) => {
 app.use("/user", user);
 app.use("/teacher", teacher);
 app.use("/student", student);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/esm-client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/esm-client/build", "index.html"));
+  });
+}
 
 
 app.listen(PORT, (req, res) => {
