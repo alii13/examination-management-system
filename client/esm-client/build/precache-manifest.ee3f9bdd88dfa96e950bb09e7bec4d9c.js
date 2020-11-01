8d29df5bca7b93900125bfe22ec2a96075eac345
alii13 <shekhaliul44@gmail.com>
2020-11-01 21:22:53 +0530

    initial commit

---
+++ client/esm-client/build/precache-manifest.ee3f9bdd88dfa96e950bb09e7bec4d9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d5a9acd4dc4af33ddff70f2d48fd4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef6f7501e4aafafb19f",
+    "url": "/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "65d3651a60be64b65fec",
+    "url": "/static/css/main.7b2e68c3.chunk.css"
+  },
+  {
+    "revision": "aef6f7501e4aafafb19f",
+    "url": "/static/js/2.7036e3f0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.7036e3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d3651a60be64b65fec",
+    "url": "/static/js/main.e4af99d8.chunk.js"
+  },
+  {
+    "revision": "6b7ffe59f62864e43d0a",
+    "url": "/static/js/runtime-main.049c5a95.js"
+  }
+]);
