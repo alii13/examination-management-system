dac421ad1c62401088de22d82e9277f71f9f136e
alii13 <shekhaliul44@gmail.com>
2020-11-01 22:10:01 +0530

    make it better

--- client/esm-client/src/store/index.js
+++ client/esm-client/src/store/index.js
@@ -1,6 +1,7 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers/index'
+
 // import logger from 'redux-logger'
 
 const allEnhancers = compose (
