5e5acfa319bbcf16bcc6549a47204c1063e399ad
alii13 <shekhaliul44@gmail.com>
2020-11-04 03:38:25 +0530

    make it better

--- client/esm-client/build/service-worker.js
+++ client/esm-client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8aa70102df8ab908b606b5a58b76fef4.js"
+  "/precache-manifest.1b8e46c36cea2a39d2c4d62c82790f03.js"
 );
 
 self.addEventListener('message', (event) => {
